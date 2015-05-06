58e31123a5cb1234b4a4f06c0292e3a05b265a8e
er0k <er0k@er0k.net>
2015-05-06 03:00:30 -0400

    fix indendation

--- ip.php
+++ ip.php
@@ -8,34 +8,34 @@ $ssid = isset($_GET['ssid']) ? true : false;
 $prefix = isset($_GET['pre']) ? $_GET['pre'] : '';
 
 if (filter_var($ip, FILTER_VALIDATE_IP)) {
-        $host = gethostbyaddr($ip);
+    $host = gethostbyaddr($ip);
 } else {
-        $ip = gethostbyname($ip);
-        $host = gethostbyaddr($ip);
+    $ip = gethostbyname($ip);
+    $host = gethostbyaddr($ip);
 }
 
 if($loc=="y") {
-        require_once('geo.php');
-        $gi = geoip_open($geoipdb,GEOIP_STANDARD);
-        $record = geoip_record_by_addr($gi,$ip);
+    require_once('geo.php');
+    $gi = geoip_open($geoipdb,GEOIP_STANDARD);
+    $record = geoip_record_by_addr($gi,$ip);
 
-        if ($txt) {
-                $output = "$ip ($host)\n";
-                $output .= $record->country_name . "\n";
-                $output .= $record->city . ' ' . $record->region . ' ' . $record->postal_code . "\n";
-        } else if ($ssid) {
-                $country = strtok(str_replace(' ', '', $record->country_name), "\n");
-                $city = str_replace(' ', '', $record->city);
-                $output = substr(str_replace(',', '', $prefix . $city . $country), 0, 32);
-        } else {
-                $output = "<p>".$ip." (".$host.")</p>\n";
-                $output .= "<p>".$record->country_name . "</p>\n";
-                $output .= "<p>".$record->city." ".$record->region." ".$record->postal_code."</p>\n";
-                $output .= '<p><a href="http://maps.google.com/maps?q='.$record->latitude.'+'.$record->longitude.'">'.$record->latitude.' '.$record->longitude.'</a></p>';
-        }
-        geoip_close($gi);
+    if ($txt) {
+        $output = "$ip ($host)\n";
+        $output .= $record->country_name . "\n";
+        $output .= $record->city . ' ' . $record->region . ' ' . $record->postal_code . "\n";
+    } else if ($ssid) {
+        $country = strtok(str_replace(' ', '', $record->country_name), "\n");
+        $city = str_replace(' ', '', $record->city);
+        $output = substr(str_replace(',', '', $prefix . $city . $country), 0, 32);
+    } else {
+        $output = "<p>".$ip." (".$host.")</p>\n";
+        $output .= "<p>".$record->country_name . "</p>\n";
+        $output .= "<p>".$record->city." ".$record->region." ".$record->postal_code."</p>\n";
+        $output .= '<p><a href="http://maps.google.com/maps?q='.$record->latitude.'+'.$record->longitude.'">'.$record->latitude.' '.$record->longitude.'</a></p>';
+    }
+    geoip_close($gi);
 
-        print $output;
+    print $output;
 
 } else {
     print $ip;
